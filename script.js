2e7b9f0c80b364a07dd3027839d7e2aedc63d30f
jedy <you@example.com>
2021-03-24 15:54:56 +0000

    popup cart

---
+++ script.js
@@ -0,0 +1,7 @@
+$(".add-to-bag").on("click", ()=>{
+    $(".container123").css("visibility", "visible")
+})
+
+$("#close-popup").on("click", ()=>{
+    $(".container123").css("visibility", "hidden")
+})
