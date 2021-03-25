9753be2128b27ec9fe79de86261bd650aa77f479
jedy <you@example.com>
2021-03-25 00:31:02 +0000

    selection of image

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ $(".add-to-bag").on("click", ()=>{
 $("#close-popup").on("click", ()=>{
     $(".container123").css("visibility", "hidden")
 })
+
+
+/*---image changing when hover on---*/
+
+var myMainImg = document.getElementById("main-img"),
+    myImg1 = document.getElementById("img1"),
+    myImg2 = document.getElementById("img2"),
+    myImg3 = document.getElementById("img3"),
+    imgNum;
+
+function ChangeImg(imgNum){
+    if(imgNum === 1)
+    {
+        myMainImg.firstElementChild.setAttribute("src",myImg1.firstElementChild.src);
+    }
+
+   else if(imgNum === 2)
+    {
+        myMainImg.firstElementChild.setAttribute("src",myImg2.firstElementChild.src);
+    }
+
+   else if(imgNum === 3)
+    {
+        myMainImg.firstElementChild.setAttribute("src",myImg3.firstElementChild.src);
+    }
+
+}
